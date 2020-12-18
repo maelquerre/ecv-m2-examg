5c23154a8bdcab8cf37b4448d56af89f5d9335b6
Maël Querré <mael.querre@gmail.com>
2020-12-18 09:54:31 +0100

    Fix logout

--- src/domain/authentication/authentication.service.js
+++ src/domain/authentication/authentication.service.js
@@ -1,55 +1,63 @@
 export function checkIfCookieExist() {
-    return document.cookie && document.cookie.split('=')[1];
+  return document.cookie && document.cookie.split('=')[1];
 }
 
 export function register(data) {
-    return fetch(`/api/register/`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
+  return fetch(`/api/register/`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  })
+    .then(async res => {
+      if (res.status !== 200 && res.status !== 201) {
+        const { message } = await res.json();
+        throw new Error(message);
+      }
+      return res;
     })
-        .then(async res => {
-            if (res.status !== 200 && res.status !== 201) {
-                const { message } = await res.json()
-                throw new Error(message)
-            }
-            return res
-        })
-        .then(res => res.json())
+    .then(res => res.json());
 }
 
 export function login(data) {
-    return fetch(`/api/login/`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
+  return fetch(`/api/login/`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  })
+    .then(async res => {
+      if (res.status !== 200 && res.status !== 201) {
+        const { message } = await res.json();
+        throw new Error(message);
+      }
+      return res;
     })
-        .then(async res => {
-            if (res.status !== 200 && res.status !== 201) {
-                const { message } = await res.json()
-                throw new Error(message)
-            }
-            return res
-        })
-        .then(res => res.json())
+    .then(res => res.json());
 }
 
 export function logout() {
-    // TODO   
+  return fetch(`/api/logout`)
+    .then(async res => {
+      if (res.status !== 200 && res.status !== 201) {
+        const { message } = await res.json();
+        throw new Error(message);
+      }
+      return res;
+    })
+    .then(res => res.json());
 }
 
 export function getMe() {
-    return fetch(`/api/me`)
-        .then(async res => {
-            if (res.status !== 200 && res.status !== 201) {
-                const { message } = await res.json()
-                throw new Error(message)
-            }
-            return res
-        })
-        .then(res => res.json());
+  return fetch(`/api/me`)
+    .then(async res => {
+      if (res.status !== 200 && res.status !== 201) {
+        const { message } = await res.json();
+        throw new Error(message);
+      }
+      return res;
+    })
+    .then(res => res.json());
 }
--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -1,61 +1,64 @@
 import { useContext, useState } from 'react';
 import { Redirect } from 'react-router-dom';
 import { ApplicationContext } from '../domain/application.store';
-import { authRegister, authLogin } from "../domain/authentication/authentication.actions";
+import { authRegister, authLogin } from '../domain/authentication/authentication.actions';
 import './Auth.css';
 
 export default function Login() {
-    const { state, dispatch } = useContext(ApplicationContext);
-    const [displayRegisterFrom, setDisplayRegisterForm] = useState(false);
-    const [form, setForm] = useState({
-        name: '',
-        email: '',
-        password: ''
+  const { state, dispatch } = useContext(ApplicationContext);
+  const [displayRegisterFrom, setDisplayRegisterForm] = useState(false);
+  const [form, setForm] = useState({
+    name: '',
+    email: '',
+    password: ''
+  });
+
+  const onChange = (e) => {
+    setForm({
+      ...form,
+      [e.target.name]: e.target.value
     });
+  };
 
-    const onChange = (e) => {
-        setForm({
-            ...form,
-            [e.target.name]: e.target.value
-        })
-    }
+  const register = (e) => {
+    e.preventDefault();
+    authRegister(dispatch, form);
+  };
 
-    const register = (e) => {
-        e.preventDefault();
-        authRegister(dispatch, form);
-    }
+  const login = (e) => {
+    e.preventDefault();
+    authLogin(dispatch, { email: form.email, password: form.password });
+  };
 
-    const login = (e) => {
-        e.preventDefault();
-        authLogin(dispatch, { email: form.email, password: form.password });
-    }
-
-    if (state.isLoggedIn) return <Redirect to="/" />
-    return (
-        <div className="auth">
-            <h1>Cocktail app !</h1>
-            <form>
-                {state.error && <p className="error">{state.error.message}</p>}
-                {displayRegisterFrom ?
-                    <>
-                        <input name="name" type="text" placeholder="Name" onChange={onChange} />
-                        <input name="email" type="email" placeholder="Email" onChange={onChange} />
-                        <input name="password" type="password" placeholder="Password" onChange={onChange} />
-                        <button onClick={register}>Register</button>
-                        <p>Have an account?</p>
-                        <p className="no-margin-top"><span className="link" onClick={() => setDisplayRegisterForm(!displayRegisterFrom)}>Login</span></p>
-                    </>
-                    :
-                    <>
-                        <input name="email" type="email" placeholder="Email" onChange={onChange} />
-                        <input name="password" type="password" placeholder="Password" onChange={onChange} />
-                        <button onClick={login}>Connexion</button>
-                        <p>Don't have an account?</p>
-                        <p className="no-margin-top"><span className="link" onClick={() => setDisplayRegisterForm(!displayRegisterFrom)}>Create your account</span></p>
-                    </>
-                }
-            </form>
-        </div>
-    )
+  if (state.isLoggedIn) return <Redirect to="/" />;
+  return (
+    <div className="auth">
+      <h1>Cocktail app !</h1>
+      <form>
+        {state.error && <p className="error">{state.error.message}</p>}
+        {displayRegisterFrom ?
+          <>
+            <input name="name" type="text" placeholder="Name" onChange={onChange} />
+            <input name="email" type="email" placeholder="Email" onChange={onChange} />
+            <input name="password" type="password" placeholder="Password" onChange={onChange} />
+            <button onClick={register}>Register</button>
+            <p>Have an account?</p>
+            <p className="no-margin-top">
+              <span className="link" onClick={() => setDisplayRegisterForm(!displayRegisterFrom)}>Login</span></p>
+          </>
+          :
+          <>
+            <input name="email" type="email" placeholder="Email" onChange={onChange} />
+            <input name="password" type="password" placeholder="Password" onChange={onChange} />
+            <button onClick={login}>Connexion</button>
+            <p>Don't have an account?</p>
+            <p className="no-margin-top">
+              <span className="link" onClick={() => setDisplayRegisterForm(!displayRegisterFrom)}>Create your account</span>
+            </p>
+          </>
+        }
+      </form>
+    </div>
+  );
 
 }
